746803bd025c08d12a9d35318d70e33a8b9388f5
Soham Anand Ladgaonkar <95856008+Soham1903@users.noreply.github.com>
2024-09-28 13:42:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 
-import Todolist from "./components/Todolist";
-import Catfact from "./components/Catfact";
+// import Todolist from "./components/Todolist";
+// import Catfact from "./components/Catfact";
 // import "./CSS/Todolist.css";
 // import "./CSS/Catfact.css";
-import Excusegen from "./components/Excusegen";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Excusegen from "./components/Excusegen";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import "./CSS/Excusegen.css";
 import PitchDetector from "./components/PitchDetector";
 import "./CSS/PitchDetector.css";
@@ -13,16 +13,17 @@ import "./CSS/PitchDetector.css";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/todo" element={<Todolist />}></Route>
-          <Route path="/catfact" element={<Catfact />}></Route>
-          <Route path="/excuse" element={<Excusegen />}></Route>
-          <Route path="/pitch" element={<PitchDetector />}></Route>
-        </Routes>
-      </Router>
+      // <Router>
+      //   <Routes>
+      //     <Route path="/todo" element={<Todolist />}></Route>
+      //     <Route path="/catfact" element={<Catfact />}></Route>
+      //     <Route path="/excuse" element={<Excusegen />}></Route>
+      //     <Route path="/pitch" element={<PitchDetector />}></Route>
+      //   </Routes>
+      // </Router>
       {/* <Todolist /> */}
       {/* <Catfact /> */}
+  < PitchDetector />
     </div>
   );
 }
